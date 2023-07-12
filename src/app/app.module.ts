07002d1030da66262e9b939e11a8649d917a80ee
Ramachandran <ramachandran.p@optisolbusiness.com>
2023-07-12 13:20:32 +0530

    alert module ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,6 +21,8 @@ import { RecipeService } from './recipes/recipe.service';
 import { AuthComponent } from 'src/app/auth/auth.component';
 import { LoadingSpinnerComponent } from './shared/loading-spinner/loading-spinner.component';
 import { AuthInterceptorService } from './auth/auth-interceptor.service';
+import { AlertComponent } from './shared/alert/alert.component';
+import { PlaceholderDirective } from './shared/placeholder/placeholder.directive';
 
 @NgModule({
   declarations: [
@@ -38,6 +40,8 @@ import { AuthInterceptorService } from './auth/auth-interceptor.service';
     RecipeEditComponent,
     AuthComponent,
     LoadingSpinnerComponent,
+    AlertComponent,
+    PlaceholderDirective,
   ],
   imports: [
     BrowserModule,
