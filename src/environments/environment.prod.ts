56835999344d6e1ddcbd19f4e8dad79159ae94b7
Ramachandran <ramachandran.p@optisolbusiness.com>
2023-07-14 10:45:56 +0530

    deployment feature added

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  fireBaseAPIkey: 'AIzaSyDvdVPDxPiJtOIOpI0LDM38AXN7RGkub1Q',
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  fireBaseAPIkey: 'AIzaSyDvdVPDxPiJtOIOpI0LDM38AXN7RGkub1Q',
+};
